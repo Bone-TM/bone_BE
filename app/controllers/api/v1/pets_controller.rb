f90e1ca48363a0b157772087c43baa33805465bb
Zachary P <zachary.s.prince@outlook.com>
2022-07-28 12:33:42 -0400

    Add create and pet_params methods

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -4,6 +4,16 @@ module Api
       def index
         render json: PetSerializer.new(Pet.all)
       end
+
+      def create
+        render json: PetSerializer.new(Pet.create(pet_params)), status: :created
+      end
+
+      private
+
+      def pet_params
+        params.require(:pet).permit(:name, :breed, :sex, :bio, :weight, :age, :user_id)
+      end
     end
   end
 end
