802773e296add11c88e807b6bc3757f94af73a20
Zachary P <zachary.s.prince@outlook.com>
2022-07-28 12:22:02 -0400

    Add create user and user_params methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,16 @@ module Api
       def index
         render json: UserSerializer.new(User.all)
       end
+
+      def create
+        render json: UserSerializer.new(User.create(user_params)), status: :created
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :bio, :email, :password_digest, :location)
+      end
     end
   end
 end
