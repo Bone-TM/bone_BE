ea47140bd9e43b8b162ff8b26c5ef7b39ee1f01a
Stirling Hostetter <stirlhoss@proton.me>
2022-08-01 15:20:43 -0500

    Test for find by email method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe User do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
   end
+
+  describe 'class methods' do
+    it '.find_by_email' do
+      user1 = create(:user, email: 'pilson@gmail.com')
+      user2 = create(:user, email: 'joeshvac@gmail.com')
+      user3 = create(:user, email: 'williesbigrigs@gmail.com')
+      user4 = create(:user, email: 'porkybob@gmail.com')
+
+      expect(User.find_by_email('p')).to eq [user1]
+      expect(User.find_by_email('po')).to eq [user4]
+      expect(User.find_by_email('w')).to eq [user3]
+      expect(User.find_by_email('J')).to eq [user2]
+      expect(User.find_by_email('hvac')).to eq [user2]
+      expect(User.find_by_email('s')).to eq [user2]
+    end
+  end
 end
