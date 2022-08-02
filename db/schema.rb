3f5302f9e52b87d36807cad0131fb6b02b6c4c29
Zach Prince <zachprince@Zachs-MBP.hitronhub.home>
2022-08-02 17:16:03 -0400

    Edit users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_165358) do
+ActiveRecord::Schema.define(version: 2022_07_27_160827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2022_07_28_165358) do
     t.string "location", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "auth_token"
   end
 
   add_foreign_key "matches", "pets"
