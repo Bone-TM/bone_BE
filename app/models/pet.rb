5318a614c226953a42616c0ff4dca0a54083d459
Tyler Ross <tross020890@gmail.com>
2022-08-02 02:13:58 -0500

    Feat: pet search find pets by multiple params

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -10,8 +10,32 @@ class Pet < ApplicationRecord
                         :sex
 
   def self.search(params)
+    @pets_all = []
+
     if params[:breed]
-      pets = Pet.where("breed ILIKE '%#{params[:breed]}'")
+      pets_breed = Pet.where("breed ILIKE '%#{params[:breed]}'")
+      @pets_all << pets_breed
     end
+    if params[:age]
+      pets_age = Pet.where("age BETWEEN '#{params[:age][:min_age]}' AND '#{params[:age][:max_age]}'")
+      @pets_all << pets_age
+    end
+    if params[:weight]
+      pets_age = Pet.where("age BETWEEN '#{params[:weight][:min_weight]}' AND '#{params[:age][:max_weight]}'")
+      @pets_all << pets_age
+    end
+    if params[:sex]
+      pets_age = Pet.where("sex = '%#{params[:sex]}'")
+      @pets_all << pets_sex
+    end
+    binding.pry
+
+    pets_final = @pets_all[0]
+    if @pets_all.count > 1
+      @pets_all.each do |pets|
+        pets_final = pets_final & pets
+      end
+    end
+    return pets_final
   end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -15,20 +15,44 @@ RSpec.describe Pet do
     it { should validate_presence_of :sex }
   end
 
-  it 'search returns pets by search criteria' do
+  it 'search returns pets by breed search criteria' do
 
     us1 = User.create(name: 'steve', email: 'steve@beve.com', bio: 'dogs r kewl', location: 'denver')
     pe1 = Pet.create(name: 'jeeve', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 5, user_id: us1.id)
     pe2 = Pet.create(name: 'cat', sex: 'male', bio: 'woof', breed: 'cocker-spaniel', weight: 19, age: 5, user_id: us1.id)
-    pe3 = Pet.create(name: 'dog', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 5, user_id: us1.id)
-    pe4 = Pet.create(name: 'cave', sex: 'male', bio: 'woof', breed: 'cocker-spaniel', weight: 19, age: 5, user_id: us1.id)
-    pe5 = Pet.create(name: 'arm', sex: 'male', bio: 'woof', breed: 'cocker-spaniel', weight: 19, age: 5, user_id: us1.id)
+    pe3 = Pet.create(name: 'dog', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 4, user_id: us1.id)
+    pe4 = Pet.create(name: 'cave', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 6, user_id: us1.id)
+    pe5 = Pet.create(name: 'arm', sex: 'male', bio: 'woof', breed: 'poodle', weight: 19, age: 3, user_id: us1.id)
+    pe5 = Pet.create(name: 'arm', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 3, user_id: us1.id)
 
     pets = Pet.search({:breed=>"husky"})
+    binding.pry
+
+    expect(pets.count).to eq(4)
+    pets.each do |pet|
+      expect(pet.breed).to eq('husky')
+    end
+
+  end
+
+  it 'search returns pets by min/max age' do
+
+    us1 = User.create(name: 'steve', email: 'steve@beve.com', bio: 'dogs r kewl', location: 'denver')
+    pe1 = Pet.create(name: 'jeeve', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 5, user_id: us1.id)
+    pe2 = Pet.create(name: 'cat', sex: 'male', bio: 'woof', breed: 'cocker-spaniel', weight: 19, age: 5, user_id: us1.id)
+    pe3 = Pet.create(name: 'dog', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 4, user_id: us1.id)
+    pe4 = Pet.create(name: 'cave', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 6, user_id: us1.id)
+    pe5 = Pet.create(name: 'arm', sex: 'male', bio: 'woof', breed: 'poodle', weight: 19, age: 3, user_id: us1.id)
+    pe5 = Pet.create(name: 'arm', sex: 'male', bio: 'woof', breed: 'husky', weight: 19, age: 3, user_id: us1.id)
+
+    pets = Pet.search({:age=>{:min_age=>4, :max_age=>5}})
+
 
     expect(pets.count).to eq(2)
-    expect(pets.first.name).to eq('jeeve')
-    expect(pets.last.name).to eq('dog')
+    pets.each do |pet|
+      expect(pet.age).to be_greater_than(3)
+      expect(pet.age).to be_less_than(6)
+    end
 
   end
 end
